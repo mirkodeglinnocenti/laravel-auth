c99097b614c717ab20fcfefc4b727efbb4390a58
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-20 17:49:42 +0200

    FEAT index view print projects date added

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,6 +1,29 @@
 @extends('layouts.app')
 @section('content')
 
-index
+<div class="container">
+    <table class="table table-striped table-inverse table-responsive">
+        <thead>
+            <tr>
+                <th>Titolo</th>
+                <th>Slug</th>
+                <th>Descrizione</th>
+                <th>Url</th>
+                <th>Azioni</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td>{{ $project->description }}</td>
+                    <td>{{ $project->url }}</td>
+                    <td>Azioni</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 
 @endsection
