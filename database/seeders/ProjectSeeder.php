1ea6ab3318cb74e4c1118a672a127bae7612d817
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-04-20 16:59:05 +0200

    FEAT migration and seeder completed added

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +16,18 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i = 0; $i < 15; $i++) {
+
+            $proj = new Project();
+
+            $proj->title = $faker->unique()->sentence($faker->numberBetween(1, 6));
+            $proj->slug = Str::of($proj->title)->slug('-');
+            $proj->description = $faker->sentence($faker->numberBetween(15, 120));
+            $proj->url = $faker->url();
+
+            $proj->save();
+        }
     }
 }
